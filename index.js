0b7947673b0c2190c68c2212951d4484a49bf731
Nelman25 <jonelvillaver735@gmail.com>
2024-11-03 04:04:43 +0800

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(express.json());
+app.use(express.static("dist"));
 app.use(cors());
 
 let notes = [
@@ -74,7 +75,7 @@ app.delete("/api/notes/:id", (req, res) => {
 	res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });

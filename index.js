b16105bc569ae0c6aaaaf4a490c242266e5e8044
Nelman25 <jonelvillaver735@gmail.com>
2024-11-08 10:44:00 +0800

    db backend integration done

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ app.get("/api/notes", (req, res) => {
 	Note.find({}).then((notes) => res.json(notes));
 });
 
+app.get("/api/notes/:id", (req, res) => {
+	Note.findById(req.params.id).then((note) => res.json(note));
+});
+
+app.post("/api/notes", (req, res) => {
+	const body = req.body;
+
+	if (body.content === undefined) {
+		return res.status(400).json({ error: "content missing" });
+	}
+
+	const note = new Note({
+		content: body.content,
+		important: body.important || false,
+	});
+
+	note.save().then((savedNote) => res.json(savedNote));
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
